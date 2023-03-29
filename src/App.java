6fcea89c017f3d06cfcc0344b59f9747e6fb5df7
lecs01 <lecs.01@outlook.com>
2023-03-28 23:39:09 -0300

    criando conexxao

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        //fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        var endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        //extrair só os dados que interessam (titulo, poster, classificação
+
+
+        //exibir e manipular os dados
+
+
 
     }
 }

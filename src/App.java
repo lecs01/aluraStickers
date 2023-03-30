f299e946534096070b29070df09ebc20d7afac0e
lecs01 <lecs.01@outlook.com>
2023-03-29 23:20:09 -0300

    criacao da classe JsonParser, extraindo dados e exibir e manipular dados

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -17,10 +19,17 @@ public class App {
         System.out.println(body);
 
         //extrair só os dados que interessam (titulo, poster, classificação
-
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        System.out.println(listaDeFilmes.size());
 
         //exibir e manipular os dados
-
+        for(Map<String, String> filme: listaDeFilmes){
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
 
 
     }

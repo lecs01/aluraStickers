ad9aa6282387036b99522be91e21648a5554965c
lecs01 <lecs.01@outlook.com>
2023-04-02 00:02:36 -0300

    aula 02

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
-import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -7,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
 
         //fazer uma conexão HTTP e buscar os top 250 filmes
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
@@ -24,13 +25,19 @@ public class App {
         System.out.println(listaDeFilmes.size());
 
         //exibir e manipular os dados
-        for(Map<String, String> filme: listaDeFilmes){
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+        var geradora = new GeradoraDeFigurinhas();
+        for (Map<String, String> filme : listaDeFilmes) {
+
+            String urlImagem =filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
         }
-
-
     }
 }
